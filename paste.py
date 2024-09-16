80892e9f19b05f5bd1ee5493106a15d152678ef0
Simon Kim <09simon@naver.com>
2024-09-16 15:13:27 +0900

    Paste Automation 3 incompletely

--- paste.py
+++ paste.py
@@ -28,17 +28,26 @@ if level==3:
     mouse.wait(button='right', target_types=('down',))
     prompt_position = position()
     print("Registered!")
+    history=dict()
     while True:
         a= input()
         if hangul.is_hangul(a):
             try:
                 file = open("Text_files\\"+a+".txt", 'r', encoding='utf-8')
+                try:
+                    history[a]+=1
+                except KeyError:
+                    history[a]=1
             except FileNotFoundError:
                 print("File not found.")
                 continue
-            text=file.readline()
-            file.close
+            for i in range(history[a]):
+                text=file.readline()
+            file.close()
         else: text=a
+        if text=='reset':
+            history=dict()
+            continue
         click(prompt_position.x, prompt_position.y ,clicks=1, interval=0, button='left')
         for i in hangul.convert(text):
             keyboard.press_and_release(i)
